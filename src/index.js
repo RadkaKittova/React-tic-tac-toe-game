fe188b2f2d5f741f15619339166d0ae0e39358fa
Radka Kittova <radka.kittova@spacesystems.cz>
2019-04-16 11:30:18 +0200

    game working without the history

--- src/index.js
+++ src/index.js
@@ -1,12 +1,127 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Square(props) {
+    return (
+        //passing the function a the onClick prop
+        <button 
+            className="square" 
+            //When a Square is clicked, the onClick function provided by the Board is called
+            onClick={props.onClick}
+            >
+                {props.value}
+        </button>
+    );
+}
+    
+class Board extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    //Boards initial stte contains arry OF 9 NULLS CORRECSPONDING TO THE 9 SQUARES
+    constructor(props) {
+        super(props);
+        this.state = {
+          squares: Array(9).fill(null),
+          xIsNext: true,
+        };
+        console.log(this.state.squares)
+      }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        // function to return early by ignoring a click if someone has won the game or if a Square is already filled
+        if (calculateWinner(squares) || squares[i]) {
+            //if no return value is specified, the function will return undefined 
+            return;
+        }
+        
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    renderSquare(i) {
+        return <Square 
+            value={this.state.squares[i]}
+            //we’ll pass down a function from the Board to the Square, and we’ll have Square call that function when a square is clicked.
+            onClick={() => this.handleClick(i)}
+        />
+    }
+
+    render() {
+        const winner =
+        calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        }
+        else {
+            status = "Next player: " + 
+            (this.state.xIsNext ? 'X' : 'O');
+        }
+    
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
